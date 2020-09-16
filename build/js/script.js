30ecabadef3f8a0402f11edc610671452a36efed
Volodymyr Honcharov <51455879+vovgoncharov@users.noreply.github.com>
2020-09-16 10:41:46 +0300

    first commit

---
+++ build/js/script.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".menu-burger-btn").on("click",function(){$(".burger-open").toggleClass("display-none"),$(".burger-close").toggleClass("display-block"),$(".menu-burger-navigation").toggleClass("display-block")})});
